0fffae648e47c1e00d4c3c143dc13cbcc3b0ce63
Ivan <ivan@Ivans-Mac-mini.local>
2024-10-11 23:08:40 -0700

    first commit

---
+++ ExploreNow/ExploreNow/ExploreNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreNowApp.swift
+//  ExploreNow
+//
+//  Created by Ivan on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct ExploreNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
