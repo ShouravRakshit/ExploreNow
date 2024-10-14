edb2eaa54bc636bce457cc44126b61d8b6455c4a
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2024-10-14 17:41:37 -0600

    Delete ExploreNow directory

--- ExploreNow/ExploreNow/Extensions/ExploreColor.swift
+++
@@ -1,9 +0,0 @@
-//  GradientBackground.swift
-//  project
-//  Completed by Qusai Dahodwalla, Shourav Rakshit Ivan, Manvi Juneja, Alisha Lalani, Alina Mansuri, Zaid Nissar, Shree Patel, Saadman Rahman, Vidhi Soni
-
-import SwiftUI
-
-extension Color {
-    static let customPurple = Color(red: 140/255, green: 82/255, blue: 255/255, opacity: 0.81)
-}
