81127dea9fbcfc60b9203f640c150e7dc6e5069f
alisha <alisha.lalani@ucalgary.ca>
2024-10-16 19:34:58 -0600

    Signup without navigation to new page

--- LBTASwiftUIFirebase 2/LBTASwiftUIFirebase/LBTASwiftUIFirebaseApp.swift
+++ LBTASwiftUIFirebase 2/LBTASwiftUIFirebase/LBTASwiftUIFirebaseApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct LBTASwiftUIFirebaseApp: App {
     var body: some Scene {
         WindowGroup {
-            MainMessagesView()
+            //MainMessagesView()
+            SignUpView()
         }
     }
 }
