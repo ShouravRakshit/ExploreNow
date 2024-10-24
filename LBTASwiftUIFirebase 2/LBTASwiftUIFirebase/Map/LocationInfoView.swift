26c951a0a7498aba7d5e1a5500e36255524e4f5a
Saadman <saadman2001rahman@gmail.com>
2024-10-24 17:30:08 -0600

    added reverse geocode location search to indicate the point of interest that the coordinates were pointing at. will change this to "name" when the post creation is added, and the location name is fetched using this.

--- LBTASwiftUIFirebase 2/LBTASwiftUIFirebase/Map/LocationInfoView.swift
+++ LBTASwiftUIFirebase 2/LBTASwiftUIFirebase/Map/LocationInfoView.swift
@@ -7,11 +7,13 @@
 
 
 import UIKit
+import MapKit
 
 class LocationInfoView: UIView {
     private let nameLabel = UILabel()
     private let ratingLabel = UILabel()
-    private let coordinatesLabel = UILabel()
+//    private let coordinatesLabel = UILabel()
+    private let poiLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,7 +33,7 @@ class LocationInfoView: UIView {
         layer.shadowOffset = CGSize(width: 0, height: 1)
         layer.shadowRadius = 4
         
-        let stackView = UIStackView(arrangedSubviews: [nameLabel, ratingLabel, coordinatesLabel])
+        let stackView = UIStackView(arrangedSubviews: [nameLabel, ratingLabel, poiLabel])
         stackView.axis = .vertical
         stackView.spacing = 4
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -49,6 +51,30 @@ class LocationInfoView: UIView {
     func configure(with location: Location) {
         nameLabel.text = location.name
         ratingLabel.text = "Rating: \(location.rating)"
-        coordinatesLabel.text = "Coordinates: \(location.coordinate.latitude), \(location.coordinate.longitude)"
+//        coordinatesLabel.text = "Coordinates: \(location.coordinate.latitude), \(location.coordinate.longitude)"
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        getPointOfInterest(for: coordinate)
+    }
+    
+    private func getPointOfInterest(for coordinate: CLLocationCoordinate2D) {
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let geocoder = CLGeocoder()
+
+        geocoder.reverseGeocodeLocation(location) { [weak self] placemarks, error in
+            guard let self = self else { return }
+            if let error = error {
+                print("Reverse geocoding error: \(error.localizedDescription)")
+                self.poiLabel.text = "Location not found"
+                return
+            }
+
+            if let placemark = placemarks?.first {
+                // Display the point of interest or the address
+                let poi = placemark.name ?? "Unknown location"
+                self.poiLabel.text = poi
+            } else {
+                self.poiLabel.text = "No POI found"
+            }
+        }
     }
 }
