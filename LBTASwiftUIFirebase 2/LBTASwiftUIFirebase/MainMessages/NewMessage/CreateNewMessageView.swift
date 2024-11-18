f1a7aac1b3e34600eb83a1e12e7178b6910a81a9
ShouravRakshit <shouravivan@gmail.com>
2024-11-18 16:30:22 -0700

    now in the search feature you can view only friends and block users will not show up there.

--- LBTASwiftUIFirebase 2/LBTASwiftUIFirebase/MainMessages/NewMessage/CreateNewMessageView.swift
+++ LBTASwiftUIFirebase 2/LBTASwiftUIFirebase/MainMessages/NewMessage/CreateNewMessageView.swift
@@ -13,39 +13,143 @@ class CreateNewMessageViewModel: ObservableObject {
     @Published var errorMessage = ""
     @Published var searchQuery = ""          // Added
     @Published var filteredUsers = [ChatUser]() // Added
+    @Published var blockedUsers: [String] = []
+    @Published var blockedByUsers: [String] = []
+
 
     init() {
-        fetchAllUsers()
+            fetchBlockedUsers {
+                self.fetchBlockedByUsers {
+                    self.fetchFriends()
+                }
+            }
+        }
+
+//    private func fetchAllUsers() {
+//        FirebaseManager.shared.firestore.collection("users").getDocuments { documentsSnapshot, error in
+//            if let error = error {
+//                self.errorMessage = "Failed to fetch users: \(error)"
+//                print("Failed to fetch users: \(error)")
+//                return
+//            }
+//
+//            var allUsers = [ChatUser]()
+//            documentsSnapshot?.documents.forEach { snapshot in
+//                let data = snapshot.data()
+//                let user = ChatUser(data: data)
+//                // Exclude the current user
+//                if user.uid != FirebaseManager.shared.auth.currentUser?.uid {
+//                    allUsers.append(user)
+//                }
+//            }
+//
+//            DispatchQueue.main.async {
+//                self.users = allUsers
+//                self.filterUsers()
+//            }
+//        }
+//    }
+    
+    func fetchBlockedByUsers(completion: @escaping () -> Void) {
+        guard let currentUserId = FirebaseManager.shared.auth.currentUser?.uid else {
+            completion()
+            return
+        }
+
+        FirebaseManager.shared.firestore.collection("blocks").document(currentUserId)
+            .getDocument { documentSnapshot, error in
+                if let error = error {
+                    print("Error fetching blockedBy users: \(error)")
+                }
+                if let data = documentSnapshot?.data() {
+                    self.blockedByUsers = data["blockedByIds"] as? [String] ?? []
+                } else {
+                    self.blockedByUsers = []
+                }
+                completion()
+            }
     }
 
-    private func fetchAllUsers() {
-        FirebaseManager.shared.firestore.collection("users").getDocuments { documentsSnapshot, error in
+    
+    func fetchUsers(withUIDs uids: [String]) {
+        var users = [ChatUser]()
+        let dispatchGroup = DispatchGroup()
+        guard let currentUserId = FirebaseManager.shared.auth.currentUser?.uid else { return }
+
+        for uid in uids {
+            // Skip if the user is in blockedByUsers
+            if self.blockedByUsers.contains(uid) {
+                continue
+            }
+
+            dispatchGroup.enter()
+            FirebaseManager.shared.firestore.collection("users").document(uid).getDocument { documentSnapshot, error in
+                if let error = error {
+                    print("Failed to fetch user with uid \(uid): \(error.localizedDescription)")
+                } else if let data = documentSnapshot?.data() {
+                    let user = ChatUser(data: data)
+                    if user.uid != currentUserId && !self.blockedUsers.contains(user.uid) {
+                        users.append(user)
+                    }
+                }
+                dispatchGroup.leave()
+            }
+        }
+
+        dispatchGroup.notify(queue: .main) {
+            self.users = users
+            self.filterUsers()
+        }
+    }
+
+
+    
+    func fetchFriends() {
+        guard let currentUserId = FirebaseManager.shared.auth.currentUser?.uid else { return }
+        let friendsRef = FirebaseManager.shared.firestore.collection("friends").document(currentUserId)
+        friendsRef.getDocument { document, error in
             if let error = error {
-                self.errorMessage = "Failed to fetch users: \(error)"
-                print("Failed to fetch users: \(error)")
+                self.errorMessage = "Failed to fetch friends: \(error.localizedDescription)"
                 return
             }
 
-            var allUsers = [ChatUser]()
-            documentsSnapshot?.documents.forEach { snapshot in
-                let data = snapshot.data()
-                let user = ChatUser(data: data)
-                // Exclude the current user
-                if user.uid != FirebaseManager.shared.auth.currentUser?.uid {
-                    allUsers.append(user)
-                }
+            guard let document = document, document.exists else {
+                self.errorMessage = "No friends list found"
+                return
             }
 
-            DispatchQueue.main.async {
-                self.users = allUsers
-                self.filterUsers()
+            if let friendUIDs = document.data()?["friends"] as? [String], !friendUIDs.isEmpty {
+                self.fetchUsers(withUIDs: friendUIDs)
+            } else {
+                self.errorMessage = "No friends found"
+                DispatchQueue.main.async {
+                    self.users = []
+                    self.filterUsers()
+                }
             }
         }
+    }
+    
+    func fetchBlockedUsers(completion: @escaping () -> Void) {
+        guard let currentUserId = FirebaseManager.shared.auth.currentUser?.uid else { return }
+
+        FirebaseManager.shared.firestore.collection("blocks").document(currentUserId)
+            .getDocument { documentSnapshot, error in
+                if let error = error {
+                    print("Error fetching blocked users: \(error)")
+                }
+                if let data = documentSnapshot?.data() {
+                    self.blockedUsers = data["blockedUserIds"] as? [String] ?? []
+                } else {
+                    self.blockedUsers = []
+                }
+                completion()
+            }
     }
 
     func filterUsers() {
         if searchQuery.isEmpty {
-            filteredUsers = []
+            filteredUsers = users
         } else {
             filteredUsers = users.filter { user in
                 user.name.lowercased().contains(searchQuery.lowercased()) ||
@@ -53,6 +157,7 @@ class CreateNewMessageViewModel: ObservableObject {
             }
         }
     }
+
 }
 
 struct CreateNewMessageView: View {
@@ -66,14 +171,9 @@ struct CreateNewMessageView: View {
         NavigationView {
             VStack {
                 searchBar
-                if vm.filteredUsers.isEmpty && !vm.searchQuery.isEmpty {
-                    Text("No users found")
-                        .foregroundColor(.gray)
-                        .padding(.top, 20)
-                    Spacer()
-                } else if vm.searchQuery.isEmpty {
-                    // Optional: Display a prompt to start typing
-                    Text("Start typing to search for users")
+
+                if vm.filteredUsers.isEmpty {
+                    Text("No friends found")
                         .foregroundColor(.gray)
                         .padding(.top, 20)
                     Spacer()
@@ -106,12 +206,11 @@ struct CreateNewMessageView: View {
                                 .padding(.vertical, 8)
                             }
                             Divider()
-                                .padding(.leading, 70) // Align divider with text
+                                .padding(.leading, 70)
                         }
                     }
                 }
             }
-            
             .toolbar {
                 ToolbarItemGroup(placement: .navigationBarLeading) {
                     Button {
@@ -135,6 +234,91 @@ struct CreateNewMessageView: View {
                 .cornerRadius(8)
                 .padding(.horizontal)
         }
-        .padding(.top, 10) // Add space between title and search bar
+        .padding(.top, 10)
     }
 }
+
+
+//struct CreateNewMessageView: View {
+//    let didSelectNewUser: (ChatUser) -> ()
+//
+//    @Environment(\.presentationMode) var presentationMode
+//
+//    @ObservedObject var vm = CreateNewMessageViewModel()
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                searchBar
+//                if vm.filteredUsers.isEmpty && !vm.searchQuery.isEmpty {
+//                    Text("No users found")
+//                        .foregroundColor(.gray)
+//                        .padding(.top, 20)
+//                    Spacer()
+//                } else if vm.searchQuery.isEmpty {
+//                    // Optional: Display a prompt to start typing
+//                    Text("Start typing to search for users")
+//                        .foregroundColor(.gray)
+//                        .padding(.top, 20)
+//                    Spacer()
+//                } else {
+//                    ScrollView {
+//                        ForEach(vm.filteredUsers) { user in
+//                            Button {
+//                                presentationMode.wrappedValue.dismiss()
+//                                didSelectNewUser(user)
+//                            } label: {
+//                                HStack {
+//                                    WebImage(url: URL(string: user.profileImageUrl))
+//                                        .resizable()
+//                                        .frame(width: 50, height: 50)
+//                                        .clipped()
+//                                        .cornerRadius(50)
+//                                        .overlay(RoundedRectangle(cornerRadius: 50)
+//                                            .stroke(Color(.label), lineWidth: 2)
+//                                        )
+//                                    VStack(alignment: .leading) {
+//                                        Text(user.name)
+//                                            .foregroundColor(Color(.label))
+//                                        Text(user.email)
+//                                            .foregroundColor(.gray)
+//                                            .font(.system(size: 12))
+//                                    }
+//                                    Spacer()
+//                                }
+//                                .padding(.horizontal)
+//                                .padding(.vertical, 8)
+//                            }
+//                            Divider()
+//                                .padding(.leading, 70) // Align divider with text
+//                        }
+//                    }
+//                }
+//            }
+//            
+//            .toolbar {
+//                ToolbarItemGroup(placement: .navigationBarLeading) {
+//                    Button {
+//                        presentationMode.wrappedValue.dismiss()
+//                    } label: {
+//                        Text("Cancel")
+//                    }
+//                }
+//            }
+//            .onChange(of: vm.searchQuery) { _ in
+//                vm.filterUsers()
+//            }
+//        }
+//    }
+//
+//    private var searchBar: some View {
+//        HStack {
+//            TextField("Search", text: $vm.searchQuery)
+//                .padding(8)
+//                .background(Color(.systemGray6))
+//                .cornerRadius(8)
+//                .padding(.horizontal)
+//        }
+//        .padding(.top, 10) // Add space between title and search bar
+//    }
+//}
