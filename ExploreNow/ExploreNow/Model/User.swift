207785fb626d92a80a401091e40675fbad3d842f
Alina <114128892+alman1@users.noreply.github.com>
2025-08-09 03:52:20 +0100

    Update User.swift

--- ExploreNow/ExploreNow/Model/User.swift
+++ ExploreNow/ExploreNow/Model/User.swift
@@ -2,7 +2,7 @@
 //  User.swift
 //  ExploreNow
 //
-//  Shourav Rakshit Ivan, Alisha Lalani, Saadman Rahman, Alina Mansuri, Manvi Juneja,
+//  Shourav Rakshit Ivan, Alisha Lalani, Saadman Rahman, -------------, Manvi Juneja,
 //  Zaid Nissar, Qusai Dahodwalla, Shree Patel, Vidhi Soni
 
 import SwiftUI
