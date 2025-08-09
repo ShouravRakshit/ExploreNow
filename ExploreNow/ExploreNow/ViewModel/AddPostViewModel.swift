ea8155f00abc41dbe7623a0c7783a93c961155c7
Alina <114128892+alman1@users.noreply.github.com>
2025-08-09 04:56:00 +0100

    Update AddPostViewModel.swift

--- ExploreNow/ExploreNow/ViewModel/AddPostViewModel.swift
+++ ExploreNow/ExploreNow/ViewModel/AddPostViewModel.swift
@@ -2,7 +2,7 @@
 //  AddPostViewModel.swift
 //  ExploreNow
 //
-//  Shourav Rakshit Ivan, Alisha Lalani, Saadman Rahman, Alina Mansuri, Manvi Juneja,
+//  Shourav Rakshit Ivan, Alisha Lalani, Saadman Rahman, -----------, Manvi Juneja,
 //  Zaid Nissar, Qusai Dahodwalla, Shree Patel, Vidhi Soni
 
 import Foundation
