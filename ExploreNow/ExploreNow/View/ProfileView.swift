0c12038d388af4f172b0ac8898314ed4b95a9234
Alina <114128892+alman1@users.noreply.github.com>
2025-08-09 04:50:21 +0100

    Update ProfileView.swift

--- ExploreNow/ExploreNow/View/ProfileView.swift
+++ ExploreNow/ExploreNow/View/ProfileView.swift
@@ -1,4 +1,4 @@
-//  Shourav Rakshit Ivan, Alisha Lalani, Saadman Rahman, Alina Mansuri, Manvi Juneja,
+//  Shourav Rakshit Ivan, Alisha Lalani, Saadman Rahman, ---------, Manvi Juneja,
 //  Zaid Nissar, Qusai Dahodwalla, Shree Patel, Vidhi Soni 
 
 import SwiftUI
